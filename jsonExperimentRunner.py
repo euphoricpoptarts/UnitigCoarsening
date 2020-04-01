0867f9a47e4326afaf43587a985ca3941a1de009
Mike Gilbert <euphoricpoptarts@gmail.com>
2020-04-01 02:04:21 -0400

    experiment

--- jsonExperimentRunner.py
+++ jsonExperimentRunner.py
@@ -8,7 +8,9 @@ import json
 from pathlib import Path
 from threading import Thread
 
-sysCall = "./sgpar_hg_exp {} {} 0 0 0 10 > /dev/null"
+parCall = "./sgpar_hg_exp {} {} 0 0 0 10 > /dev/null"
+serialCall = "./sgpar_hg_serial {} {} {} 0 0 10 > /dev/null"
+set_threads = "export OMP_NUM_THREADS={}"
 
 def printStat(fieldTitle, statList, outfile):
     min_s = min(statList)
@@ -18,43 +20,70 @@ def printStat(fieldTitle, statList, outfile):
     med = median(statList)
     print("{}: mean={}, median={}, min={}, max={}, std-dev={}".format(fieldTitle, avg, med, min_s, max_s, sdev), file=outfile)
 
-def processGraph(filepath, metricDir, logFile):
+def analyzeMetrics(metricsPath, logFile):
+    with open(metricsPath) as fp:
+            data = json.load(fp)
+
+    times = [d['total-duration-seconds'] for d in data]
+    coarsenTimes = [d['coarsen-duration-seconds'] for d in data]
+    coarsenSortTimes = [d['coarsen-sort-duration-seconds'] for d in data]
+    refineTimes = [d['refine-duration-seconds'] for d in data]
+    edgeCuts = [d['edge-cut'] for d in data]
+    coarseLevels = list(zip(*[d['coarse-levels'] for d in data]))
+    numCoarseLevels = [d['number-coarse-levels'] for d in data]
+    coarsenNonSortTimes = [i - j for i, j in zip(coarsenTimes, coarsenSortTimes)]
+
+    with open(logFile, "w") as output:
+        printStat("Total duration", times, output)
+        printStat("Coarsening duration", coarsenTimes, output)
+        printStat("Coarsening sort duration", coarsenSortTimes, output)
+        printStat("Coarsening other duration", coarsenNonSortTimes, output)
+        printStat("Refine duration", refineTimes, output)
+        printStat("Edge cut", edgeCuts, output)
+        printStat("Coarse levels", numCoarseLevels, output)
+        coarseLevel = len(coarseLevels) - 1
+        for level in coarseLevels:
+            printStat("Coarse level {} refine iterations".format(coarseLevel), [l['refine-iterations'] for l in level], output)
+            printStat("Coarse level {} unrefined edge cuts".format(coarseLevel), [l['unrefined-edge-cut'] for l in level], output)
+            coarseLevel = coarseLevel - 1
+
+def processGraph(filepath, metricDir, logFileTemplate):
     
+    #parallel HEC
     metricsPath = "{}/group{}.txt".format(metricDir, secrets.token_urlsafe(10))
-    print("running sgpar on {}, data logged in {}".format(filepath, metricsPath))
-
-    err = os.system(sysCall.format(filepath, metricsPath))
-
+    print("running parallel HEC sgpar on {}, data logged in {}".format(filepath, metricsPath))
+    err = os.system(parCall.format(filepath, metricsPath))
     if(err == 256):
         print("error code: {}".format(err))
         print("error produced by:")
-        print(sysCall.format(filepath, metricsPath, bestPart, tolerance))
+        print(parCall.format(filepath, metricsPath))
     else:
+        logFile = logFileTemplate.format("parHEC")
+        analyzeMetrics(metricsPath, logFile)
 
-        cnt = 0
-        with open(metricsPath) as fp:
-            data = json.load(fp)
+    #serial HEC
+    metricsPath = "{}/group{}.txt".format(metricDir, secrets.token_urlsafe(10))
+    print("running serial HEC sgpar on {}, data logged in {}".format(filepath, metricsPath))
+    err = os.system(serialCall.format(filepath, metricsPath, 0))
+    if(err == 256):
+        print("error code: {}".format(err))
+        print("error produced by:")
+        print(serialCall.format(filepath, metricsPath, 0))
+    else:
+        logFile = logFileTemplate.format("serialHEC")
+        analyzeMetrics(metricsPath, logFile)
 
-        times = [d['total-duration-seconds'] for d in data]
-        coarsenTimes = [d['coarsen-duration-seconds'] for d in data]
-        coarsenSortTimes = [d['coarsen-sort-duration-seconds'] for d in data]
-        refineTimes = [d['refine-duration-seconds'] for d in data]
-        edgeCuts = [d['edge-cut'] for d in data]
-        coarseLevels = list(zip(*[d['coarse-levels'] for d in data]))
-        numCoarseLevels = [d['number-coarse-levels'] for d in data]
-
-        with open(logFile, "w") as output:
-            printStat("Total duration", times, output)
-            printStat("Coarsening duration", coarsenTimes, output)
-            printStat("Coarsening sort duration", coarsenSortTimes, output)
-            printStat("Refine duration", refineTimes, output)
-            printStat("Edge cut", edgeCuts, output)
-            printStat("Coarse levels", numCoarseLevels, output)
-            coarseLevel = len(coarseLevels) - 1
-            for level in coarseLevels:
-                printStat("Coarse level {} refine iterations".format(coarseLevel), [l['refine-iterations'] for l in level], output)
-                printStat("Coarse level {} unrefined edge cuts".format(coarseLevel), [l['unrefined-edge-cut'] for l in level], output)
-                coarseLevel = coarseLevel - 1
+    #serial matching
+    metricsPath = "{}/group{}.txt".format(metricDir, secrets.token_urlsafe(10))
+    print("running serial match sgpar on {}, data logged in {}".format(filepath, metricsPath))
+    err = os.system(serialCall.format(filepath, metricsPath, 1))
+    if(err == 256):
+        print("error code: {}".format(err))
+        print("error produced by:")
+        print(serialCall.format(filepath, metricsPath, 1))
+    else:
+        logFile = logFileTemplate.format("serialMatch")
+        analyzeMetrics(metricsPath, logFile)
 
     print("end {} processing".format(filepath))
 
@@ -68,7 +97,8 @@ def main():
     for file in glob(globMatch):
         filepath = file
         stem = Path(filepath).stem
-        logFile = "{}/{}_Sampling_Data.txt".format(logDir, stem)
+        #will fill in the third argument later
+        logFile = "{}/{}_{}_Sampling_Data.txt".format(logDir, stem,"{}")
         processGraph(filepath, metricDir, logFile)
 
 
