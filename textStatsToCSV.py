1b370b5f20b6f18a8095fe087e83f7b534846693
Mike Gilbert <euphoricpoptarts@gmail.com>
2020-08-03 13:37:40 -0400

    blah

--- textStatsToCSV.py
+++ textStatsToCSV.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import re
 from glob import glob
 from parse import parse
 from statistics import mean, stdev, median
@@ -8,14 +9,9 @@ import json
 from pathlib import Path
 from itertools import zip_longest
 
+stemParse = r"(.+)_(HEC|MIS|mt|match)_Sampling_Data"
 lineParse = "{}mean={}, median={}, min={}, max={}, std-dev={}"
-fieldHeaders = "\
-graph experiment, median time, \
-median coarsen time, \
-median coarsen map time, \
-median coarsen sort time, \
-median refine time, \
-median edge cut, "
+fieldHeaders = "Graph, HEC, MIS, Match, MT"
 
 fieldHeaders2 = "\
 graph experiment, mean time, median time, min time, max time, time std-dev, \
@@ -25,8 +21,8 @@ mean coarsen other time, median coarsen other time, min coarsen other time, max 
 mean refine time, median refine time, min refine time, max refine time, refine time std-dev, \
 mean edge cut, median edge cut, min edge cut, max edge cut, edge cut std-dev"
 
-def textStatsToCSV(stem, filepath):
-    graphStats = [stem]
+def textStatsToCSV(stem, filepath, relevantLines):
+    graphStats = []
     parsedLines = []
     with open(filepath,"r") as f:
         for line in f:
@@ -36,7 +32,7 @@ def textStatsToCSV(stem, filepath):
     if len(parsedLines) < 11:
         return stem
 
-    for line in [0,1,2,3,8,9]:
+    for line in relevantLines:
         parsed = parsedLines[line]
         if parsed is not None:
             parsed = list(parsed)
@@ -45,7 +41,7 @@ def textStatsToCSV(stem, filepath):
         else:
             for stat in [2]:
                 graphStats.append("nan")
-    return ",".join(graphStats)
+    return graphStats
 
 def main():
 
@@ -54,16 +50,32 @@ def main():
 
     globMatch = "{}/*.txt".format(logDir)
 
-    data = []
+    data = {}
     for file in glob(globMatch):
         filepath = file
         stem = Path(filepath).stem
-        data.append(textStatsToCSV(stem, filepath))
+        stemMatch = re.match(stemParse, stem)
+        if stemMatch is not None:
+            graph = stemMatch.groups()[0]
+            experiment = stemMatch.groups()[1]
+            if graph not in data:
+                data[graph] = {}
+            if experiment in ["HEC", "MIS"]:
+                data[graph][experiment] = textStatsToCSV(stem, filepath, [0,1,2,3,8,9])
+            else:
+                data[graph][experiment] = textStatsToCSV(stem, filepath, [0,1,2,3,12,13])
 
+    experiments = ["HEC", "MIS", "match", "mt"]
     with open(outFile,"w") as f:
         print(fieldHeaders, file=f)
-        for datum in data:
-            print(datum, file=f)
+        for graph, values in data.items():
+            l = [graph]
+            for experiment in experiments:
+                if experiment in values:
+                    l.append(values[experiment][5])
+                else:
+                    l.append("DNF")
+            print(",".join(l), file=f)
 
 
 if __name__ == "__main__":
